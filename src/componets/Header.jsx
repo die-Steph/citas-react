f42ea44544410a69f266f12a8b986af66d9bb42d
¨EibarDH¨ <ing.eibardh@yahoo.com>
2023-02-03 16:14:48 +0100

    solo se cambio el header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <h1 className="font-black text-5xl text-center mx-auto md:w-2/3">
-      Seguimientos Pacientes{' '}
+      Seguimientos Pacientes{' '} con GitHub {' git'}
       <span className="text-indigo-600">Veterinaria</span>
     </h1>
   );
